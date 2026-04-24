88bc9ea8f9edd7fb4407d76b8cc504d190e5d21e
Rizwan-090505 <rizzwanabdullah@gmail.com>
2026-04-24 21:56:59 +0500

    fix:slow load fixup

--- src/rag/model_loader.py
+++ src/rag/model_loader.py
@@ -1,7 +1,7 @@
 from functools import lru_cache
-from sentence_transformers import SentenceTransformer
 
 
 @lru_cache(maxsize=1)
 def _get_model(model_name: str):
+    from sentence_transformers import SentenceTransformer
     return SentenceTransformer(model_name)
